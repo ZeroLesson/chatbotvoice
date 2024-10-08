b5e514f2e6bdb5e1ffdcf526ba4afadd61925d41
Malc <115590219+Malckub@users.noreply.github.com>
2024-10-08 18:10:14 +0700

    update

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from myapp import views
-
 urlpatterns = [
-    path('',views.index),
+    path('',views.index,name="/"),
+    path('api',views.api,name="api"),
     
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
-
+import requests
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    if request.method == "POST":
+        email = request.POST["email"]
+        password = request.POST["password"]
+        print(email , password)
+        if email == "banyawat.t@ku.th" and password == "Ban@071145":
+            return redirect('api')
+        else:
+            return redirect('/')
+    else:
+        return render(request, "index.html")
+
+def api(request):
+    api_url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(api_url)
+    if response.status_code == 200:
+            # ดึงข้อมูล JSON จาก API
+        data = response.json()
+    else:
+        data = {'error': 'Failed to retrieve data'}
+
+    # ส่งข้อมูลไปยังเทมเพลต หรือแสดงผลเป็น JSON
+    return render(request, 'api.html', {'data': data})
